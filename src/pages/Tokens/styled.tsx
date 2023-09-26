f9a4a336cfcc605bb88d65081dad76c6d442bb73
WhiteMind <WhiteMind@qq.com>
2023-09-26 18:41:38 +0800

    fix: stylelint ineffectiveness and differences in stylelint execution on different os

--- src/pages/Tokens/styled.tsx
+++ src/pages/Tokens/styled.tsx
@@ -66,19 +66,19 @@ export const TokensTableTitle = styled.div`
   }
 
   @media (max-width: 1000px) {
-    >span: nth-child(1) {
+    > span:nth-child(1) {
       flex: 3.4;
     }
 
-    >span: nth-child(2) {
+    > span:nth-child(2) {
       flex: 2.5;
     }
 
-    >span: nth-child(3) {
+    > span:nth-child(3) {
       flex: 2.1;
     }
 
-    >span: nth-child(4) {
+    > span:nth-child(4) {
       flex: 2;
     }
   }
