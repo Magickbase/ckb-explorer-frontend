e0dfb91e5aec6e142f0b01325908fbb7ae3256e7
WhiteMind <WhiteMind@qq.com>
2023-12-01 11:19:07 +0800

    refactor: move useDASAccount to a more appropriate location

--- src/pages/Address/index.tsx
+++ src/pages/Address/index.tsx
@@ -13,7 +13,7 @@ import { isAxiosError } from '../../utils/error'
 import { Card, HashCardHeader } from '../../components/Card'
 import { ReactComponent as ShareIcon } from './share.svg'
 import styles from './styles.module.scss'
-import { useDASAccount } from '../../contexts/providers/dasQuery'
+import { useDASAccount } from '../../hooks/useDASAccount'
 
 export const Address = () => {
   const { address } = useParams<{ address: string }>()
