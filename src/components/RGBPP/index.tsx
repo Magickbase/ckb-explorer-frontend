19a0d1ad972a48a2887b0e860503a3d26ee6a2f8
Keith <keithwhisper@gmail.com>
2024-03-28 19:41:47 +0900

    fix: fix direction of leap in rgb++ dialog

--- src/components/RGBPP/index.tsx
+++ src/components/RGBPP/index.tsx
@@ -1,5 +1,4 @@
-import { useState } from 'react'
-import { addressToScript } from '@nervosnetwork/ckb-sdk-utils'
+import { useMemo, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Transaction } from '../../models/Transaction'
 import SimpleModal from '../Modal'
@@ -7,26 +6,23 @@ import SimpleButton from '../SimpleButton'
 import TransactionRGBPPDigestModal from '../TransactionItem/TransactionRGBPPDigestModal'
 import styles from './styles.module.scss'
 import { TransactionLeapDirection } from './types'
-import { Cell } from '../../models/Cell'
-import { matchScript } from '../../utils/util'
-
-const computeRGBPPCellAmount = (cells: Cell[]) =>
-  cells.filter(cell => {
-    try {
-      const script = addressToScript(cell.addressHash)
-      const tag = matchScript(script.codeHash, script.hashType)
-      return tag?.tag === 'rgb++'
-    } catch (e) {
-      return false
-    }
-  }).length
 
 const RGBPP = ({ transaction }: { transaction: Transaction }) => {
   const [showModal, setShowModal] = useState(false)
   const { t } = useTranslation()
 
-  const inputRGBAmount = computeRGBPPCellAmount(transaction.displayInputs)
-  const outputRGBAmount = computeRGBPPCellAmount(transaction.displayOutputs)
+  const direction = useMemo(() => {
+    // FIXME: should be from API because inputs/outputs are paginated
+    const inputCellCount = transaction.displayInputs.filter(c => c.rgbInfo).length
+    const outputCellCount = transaction.displayOutputs.filter(c => c.rgbInfo).length
+    if (inputCellCount === outputCellCount) {
+      return TransactionLeapDirection.NONE
+    }
+    if (inputCellCount > outputCellCount) {
+      return TransactionLeapDirection.OUT
+    }
+    return TransactionLeapDirection.IN
+  }, [transaction])
 
   return (
     <div>
@@ -43,7 +39,7 @@ const RGBPP = ({ transaction }: { transaction: Transaction }) => {
         <TransactionRGBPPDigestModal
           onClickClose={() => setShowModal(false)}
           hash={transaction.transactionHash}
-          leapDirection={inputRGBAmount > outputRGBAmount ? TransactionLeapDirection.OUT : TransactionLeapDirection.IN}
+          leapDirection={direction}
         />
       </SimpleModal>
     </div>
