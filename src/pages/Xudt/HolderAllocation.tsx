f58f4d93eeec16ea28914e166aca14aa97c7287d
daryl <sai9375@hotmail.com>
2024-06-21 13:44:16 +0800

    feat(holder-allocation): api

--- src/pages/Xudt/HolderAllocation.tsx
+++ src/pages/Xudt/HolderAllocation.tsx
@@ -1,74 +1,67 @@
 import { useTranslation } from 'react-i18next'
+import { MouseEventHandler } from 'react'
 import styles from './HolderAllocation.module.scss'
+import { localeNumberString } from '../../utils/number'
+import CloseIcon from '../../assets/modal_close.png'
+import SimpleButton from '../../components/SimpleButton'
 
-const HolderAllocation = ({ typeHash }: { typeHash: string }) => {
+const HolderAllocation = ({
+  ckbHolderAmount,
+  btcHolderAmount,
+  lockHoderAmount,
+  onClose,
+}: {
+  ckbHolderAmount: string
+  btcHolderAmount: string
+  lockHoderAmount?: {
+    lock: string
+    holderAmount: string
+  }[]
+  onClose: MouseEventHandler<HTMLDivElement>
+}) => {
   const [t] = useTranslation()
   return (
     <div className={styles.holderAllocationContainer}>
-      <h2>{t('xudt.holder_allocation')}</h2>
-      <p>
-        {t('xudt.holder_allocation_description', {
-          ckb: '1,000',
-          btc: '200',
-        })}
-        {typeHash}
-      </p>
-      <div className={styles.table}>
-        <table>
-          <thead>
-            <tr>
-              <td>
-                <div>{t('xudt.lock_hash')}</div>
-              </td>
-              <td>
-                <div>{t('xudt.count')}</div>
-              </td>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>
-                <div>secp256k1</div>
-              </td>
-              <td>
-                <div>200</div>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <div>omnilock</div>
-              </td>
-              <td>
-                <div>400</div>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <div>joyid</div>
-              </td>
-              <td>
-                <div>100</div>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <div>RGB++</div>
-              </td>
-              <td>
-                <div>400</div>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <div>other</div>
-              </td>
-              <td>
-                <div>300</div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+      <div className={styles.holderAllocationContent}>
+        <h2>{t('xudt.holder_allocation')}</h2>
+        <p>
+          {t('xudt.holder_allocation_description', {
+            ckb: localeNumberString(ckbHolderAmount),
+            btc: localeNumberString(btcHolderAmount),
+          })}
+        </p>
+        {lockHoderAmount && (
+          <div className={styles.table}>
+            <table>
+              <thead>
+                <tr>
+                  <td>
+                    <div>{t('xudt.lock_hash')}</div>
+                  </td>
+                  <td>
+                    <div>{t('xudt.count')}</div>
+                  </td>
+                </tr>
+              </thead>
+              <tbody>
+                {lockHoderAmount.map(amount => (
+                  <tr>
+                    <td>
+                      <div>{amount.lock}</div>
+                    </td>
+                    <td>
+                      <div>{amount.holderAmount}</div>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
       </div>
+      <SimpleButton onClick={onClose} className={styles.closeIcon}>
+        <img src={CloseIcon} alt="close icon" />
+      </SimpleButton>
     </div>
   )
 }
