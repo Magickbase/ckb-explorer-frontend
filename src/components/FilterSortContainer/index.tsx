9a372c5fe797fb671aa9fb7da3ef10c457a0bc9a
daryl <sai9375@hotmail.com>
2024-07-02 16:42:09 +0800

    feat(filter-card): two columns

--- src/components/FilterSortContainer/index.tsx
+++ src/components/FilterSortContainer/index.tsx
@@ -1,11 +1,24 @@
 import classNames from 'classnames'
-import { ComponentProps, FC } from 'react'
+import { ComponentProps, FC, ReactNode } from 'react'
 import styles from './index.module.scss'
 
 export const FilterSortContainerOnMobile: FC<ComponentProps<'div'>> = ({ children, ...elProps }) => {
+  const { left, right } = Array.from(children as Iterable<ReactNode>).reduce<{ left: ReactNode[]; right: ReactNode[] }>(
+    (acc, child, index) => {
+      if (index % 2 === 0) {
+        acc.left.push(child)
+      } else {
+        acc.right.push(child)
+      }
+      return acc
+    },
+    { left: [], right: [] },
+  )
+
   return (
     <div {...elProps} className={classNames(styles.filterSortContainerOnMobile, elProps.className)}>
-      {children}
+      <div className={styles.col}>{left}</div>
+      <div className={styles.col}>{right}</div>
     </div>
   )
 }
