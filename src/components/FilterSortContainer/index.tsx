509ae3be099993a63b3beac34cf9b97c5d1d223b
daryl <sai9375@hotmail.com>
2024-07-09 16:55:10 +0800

    feat(nft): change way for two column

--- src/components/FilterSortContainer/index.tsx
+++ src/components/FilterSortContainer/index.tsx
@@ -3,22 +3,13 @@ import { ComponentProps, FC, ReactNode } from 'react'
 import styles from './index.module.scss'
 
 export const FilterSortContainerOnMobile: FC<ComponentProps<'div'>> = ({ children, ...elProps }) => {
-  const { left, right } = Array.from(children as Iterable<ReactNode>).reduce<{ left: ReactNode[]; right: ReactNode[] }>(
-    (acc, child, index) => {
-      if (index % 2 === 0) {
-        acc.left.push(child)
-      } else {
-        acc.right.push(child)
-      }
-      return acc
-    },
-    { left: [], right: [] },
-  )
-
   return (
     <div {...elProps} className={classNames(styles.filterSortContainerOnMobile, elProps.className)}>
-      <div className={styles.col}>{left}</div>
-      <div className={styles.col}>{right}</div>
+      <ul>
+        {Array.from(children as Iterable<ReactNode>).map(child => (
+          <li>{child}</li>
+        ))}
+      </ul>
     </div>
   )
 }
