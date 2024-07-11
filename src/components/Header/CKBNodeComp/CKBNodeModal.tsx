5c8422b261100a3e439463531d3adde3cd4f10c1
painterpuppets <painterpuppets@gmail.com>
2024-07-11 16:23:56 +0800

    feat: support custom rpc endpoint

--- src/components/Header/CKBNodeComp/CKBNodeModal.tsx
+++ src/components/Header/CKBNodeComp/CKBNodeModal.tsx
@@ -1,39 +1,171 @@
-import { useRef } from 'react'
+import { FC, PropsWithChildren, useRef, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useCKBNode } from '../../../hooks/useCKBNode'
 import CommonModal from '../../CommonModal'
 import { HelpTip } from '../../HelpTip'
 import { Switch } from '../../ui/Switch'
 import CloseIcon from '../../../assets/modal_close.png'
+import { ReactComponent as EditIcon } from './edit.svg'
+import { ReactComponent as DeleteIcon } from './delete.svg'
+
+import BackIcon from './back.png'
 import styles from './style.module.scss'
+
+const NodeEditForm: FC<
+  PropsWithChildren<{
+    initialData?: { alias: string; url: string }
+    onSubmit: (data: { alias: string; url: string }) => void
+  }>
+> = ({ children, initialData, onSubmit }) => {
+  const [alias, setAlias] = useState(initialData?.alias ?? '')
+  const [url, setUrl] = useState(initialData?.url ?? '')
+
+  return (
+    <form
+      className={styles.nodeEditForm}
+      onSubmit={e => {
+        e.preventDefault()
+        e.stopPropagation()
+        onSubmit({ alias, url })
+      }}
+    >
+      <div>
+        <label>Alias</label>
+        <input value={alias} onChange={e => setAlias(e.target.value)} />
+      </div>
+
+      <div>
+        <label>RPC URL</label>
+        <input value={url} onChange={e => setUrl(e.target.value)} />
+      </div>
+
+      {children}
+    </form>
+  )
+}
 
 export const CKBNodeModal = ({ onClose }: { onClose: () => void }) => {
   const { t } = useTranslation()
   const ref = useRef<HTMLDivElement>(null)
-  const { isActivated, setIsActivated } = useCKBNode()
+  const { isActivated, setIsActivated, nodeService, nodes, addNode, removeNode, switchNode, editNode } = useCKBNode()
+  const [panel, setPanel] = useState<'main' | 'add' | 'edit'>('main')
+  const [editIndex, setEditIndex] = useState(0)
+
+  const NodeEditPanel = () => (
+    <div className={styles.contentWrapper}>
+      <div className={styles.modalTitle}>
+        <button type="button" onClick={() => setPanel('main')} className={styles.closeBtn}>
+          <img src={BackIcon} alt="back icon" />
+        </button>
+
+        <p>{t('node.modify_node')}</p>
+        <button type="button" onClick={onClose} className={styles.closeBtn}>
+          <img src={CloseIcon} alt="close icon" />
+        </button>
+      </div>
+
+      <NodeEditForm
+        initialData={nodes[editIndex]}
+        onSubmit={data => {
+          editNode(editIndex, data)
+          setPanel('main')
+        }}
+      >
+        <button type="submit" className={styles.btn}>
+          {t('node.modify_node')}
+        </button>
+      </NodeEditForm>
+    </div>
+  )
+
+  const NodeMainPanel = () => (
+    <div className={styles.contentWrapper}>
+      <div className={styles.modalTitle}>
+        <p>{t('navbar.node')}</p>
+        <button type="button" onClick={onClose} className={styles.closeBtn}>
+          <img src={CloseIcon} alt="close icon" />
+        </button>
+      </div>
+
+      <div className={styles.switcher}>
+        <label htmlFor="node-connect-mode">{t('node.node_connect_mode')}</label>
+        <HelpTip title={t('node.node_connect_tooltip')} />
+        <Switch
+          id="node-connect-mode"
+          style={{ marginLeft: 'auto' }}
+          checked={isActivated}
+          onCheckedChange={checked => setIsActivated(checked)}
+        />
+      </div>
+
+      <div className={styles.nodeList}>
+        {nodes.map((n, index) => (
+          <div className={styles.node} key={n.url}>
+            <div className={styles.nodeTitle}>
+              <input onClick={() => switchNode(n.url)} type="checkbox" checked={nodeService.nodeEndpoint === n.url} />
+              <div className={styles.nodeAlias}>{n.alias}</div>
+              {index !== 0 && (
+                <>
+                  {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events */}
+                  <span
+                    onClick={() => {
+                      setPanel('edit')
+                      setEditIndex(index)
+                    }}
+                    style={{ marginLeft: 'auto' }}
+                  >
+                    <EditIcon width={16} />
+                  </span>
+                  {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events */}
+                  <span onClick={() => removeNode(n.url)}>
+                    <DeleteIcon width={16} />
+                  </span>
+                </>
+              )}
+            </div>
+            <div className={styles.nodeSubtitle}>({n.url})</div>
+          </div>
+        ))}
+      </div>
+
+      <button type="button" className={styles.btn} style={{ width: '100%' }} onClick={() => setPanel('add')}>
+        {t('node.add_node')}
+      </button>
+    </div>
+  )
+
+  const NodeAddPanel = () => (
+    <div className={styles.contentWrapper}>
+      <div className={styles.modalTitle}>
+        <button type="button" onClick={() => setPanel('main')} className={styles.closeBtn}>
+          <img src={BackIcon} alt="back icon" />
+        </button>
+
+        <p>{t('node.add_node')}</p>
+        <button type="button" onClick={onClose} className={styles.closeBtn}>
+          <img src={CloseIcon} alt="close icon" />
+        </button>
+      </div>
+
+      <NodeEditForm
+        onSubmit={data => {
+          addNode({ alias: data.alias, url: data.url })
+          setPanel('main')
+        }}
+      >
+        <button type="submit" className={styles.btn}>
+          {t('node.add_node')}
+        </button>
+      </NodeEditForm>
+    </div>
+  )
 
   return (
     <CommonModal isOpen onClose={onClose}>
       <div ref={ref} className={styles.modalWrapper}>
-        <div className={styles.contentWrapper}>
-          <div className={styles.modalTitle}>
-            <p>{t('navbar.node')}</p>
-            <button type="button" onClick={onClose} className={styles.closeBtn}>
-              <img src={CloseIcon} alt="close icon" />
-            </button>
-          </div>
-
-          <div className={styles.switcher}>
-            <label htmlFor="node-connect-mode">{t('node.node_connect_mode')}</label>
-            <HelpTip title={t('node.node_connect_tooltip')} />
-            <Switch
-              id="node-connect-mode"
-              style={{ marginLeft: 'auto' }}
-              checked={isActivated}
-              onCheckedChange={checked => setIsActivated(checked)}
-            />
-          </div>
-        </div>
+        {panel === 'main' && <NodeMainPanel />}
+        {panel === 'add' && <NodeAddPanel />}
+        {panel === 'edit' && <NodeEditPanel />}
       </div>
     </CommonModal>
   )
--- src/hooks/useCKBNode.tsx
+++ src/hooks/useCKBNode.tsx
@@ -2,10 +2,34 @@ import { useContext, createContext, useState, PropsWithChildren } from 'react'
 import { NodeService } from '../services/NodeService'
 
 const NODE_CONNECT_MODE_KEY = 'node_connect_mode'
+const NODE_ENDPOINTS = 'node_endpoints'
+
+const loadEndpoints = (): CKBNode[] => {
+  const item = localStorage.getItem(NODE_ENDPOINTS)
+  try {
+    return item ? JSON.parse(item) : []
+  } catch {
+    return []
+  }
+}
+
+const saveEndpoints = (nodes: CKBNode[]) => {
+  localStorage.setItem(NODE_ENDPOINTS, JSON.stringify(nodes))
+}
+
+export interface CKBNode {
+  alias: string
+  url: string
+}
 
 export interface ICKBNodeContext {
+  nodes: CKBNode[]
   nodeService: NodeService
   isActivated: boolean
+  addNode: (node: CKBNode) => void
+  removeNode: (url: string) => void
+  editNode: (index: number, node: CKBNode) => void
+  switchNode: (url: string) => void
   setIsActivated: (isActivated: boolean) => void
 }
 
@@ -24,7 +48,8 @@ interface CKBNodeProviderProps {
 }
 
 export const CKBNodeProvider = ({ children, defaultEndpoint }: PropsWithChildren<CKBNodeProviderProps>) => {
-  const nodeService = new NodeService(defaultEndpoint)
+  const [nodeService, setNodeService] = useState(new NodeService(defaultEndpoint))
+  const [nodes, _setNodes] = useState<CKBNode[]>(loadEndpoints())
   const [isActivated, _setIsActivated] = useState(localStorage.getItem(NODE_CONNECT_MODE_KEY) === 'true')
 
   const setIsActivated = (value: boolean) => {
@@ -32,7 +57,50 @@ export const CKBNodeProvider = ({ children, defaultEndpoint }: PropsWithChildren
     _setIsActivated(value)
   }
 
+  const setNodes = (nodes: CKBNode[]) => {
+    _setNodes(nodes)
+    saveEndpoints(nodes)
+  }
+
+  const addNode = (node: CKBNode) => {
+    setNodes([...nodes, node])
+  }
+
+  const removeNode = (url: string) => {
+    setNodes(nodes.filter(node => node.url !== url))
+  }
+
+  const editNode = (index: number, node: CKBNode) => {
+    const newNodes = [...nodes]
+    if (!newNodes[index - 1]) return
+    newNodes[index - 1] = node
+    setNodes(newNodes)
+  }
+
+  const switchNode = (url: string) => {
+    setNodeService(new NodeService(url))
+  }
+
   return (
-    <CKBNodeContext.Provider value={{ nodeService, isActivated, setIsActivated }}>{children}</CKBNodeContext.Provider>
+    <CKBNodeContext.Provider
+      value={{
+        nodes: [
+          {
+            alias: 'Default Node',
+            url: defaultEndpoint,
+          },
+          ...nodes,
+        ],
+        addNode,
+        removeNode,
+        editNode,
+        switchNode,
+        nodeService,
+        isActivated,
+        setIsActivated,
+      }}
+    >
+      {children}
+    </CKBNodeContext.Provider>
   )
 }
