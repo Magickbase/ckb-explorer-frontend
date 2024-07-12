766a39729fd31ed6de1be35acb5f8e361e6f0bd5
painterpuppets <painterpuppets@gmail.com>
2024-07-12 12:50:12 +0800

    feat: switch network before check chain info

--- src/components/Header/CKBNodeComp/CKBNodeModal.tsx
+++ src/components/Header/CKBNodeComp/CKBNodeModal.tsx
@@ -7,9 +7,21 @@ import { Switch } from '../../ui/Switch'
 import CloseIcon from '../../../assets/modal_close.png'
 import { ReactComponent as EditIcon } from './edit.svg'
 import { ReactComponent as DeleteIcon } from './delete.svg'
-
+import { useSetToast } from '../../Toast'
 import BackIcon from './back.png'
 import styles from './style.module.scss'
+
+function isValidHttpUrl(str: string) {
+  let url
+
+  try {
+    url = new URL(str)
+  } catch (_) {
+    return false
+  }
+
+  return url.protocol === 'http:' || url.protocol === 'https:'
+}
 
 const NodeEditForm: FC<
   PropsWithChildren<{
@@ -17,8 +29,8 @@ const NodeEditForm: FC<
     onSubmit: (data: { alias: string; url: string }) => void
   }>
 > = ({ children, initialData, onSubmit }) => {
-  const [alias, setAlias] = useState(initialData?.alias ?? '')
-  const [url, setUrl] = useState(initialData?.url ?? '')
+  const { t } = useTranslation()
+  const setToast = useSetToast()
 
   return (
     <form
@@ -26,17 +38,35 @@ const NodeEditForm: FC<
       onSubmit={e => {
         e.preventDefault()
         e.stopPropagation()
+        const alias = e.currentTarget.alias.value
+        const url = e.currentTarget.url.value
+
+        if (typeof alias !== 'string') {
+          setToast({ message: t('node.alias_required'), type: 'danger' })
+          return
+        }
+
+        if (typeof url !== 'string') {
+          setToast({ message: t('node.url_required'), type: 'danger' })
+          return
+        }
+
+        if (!isValidHttpUrl(url)) {
+          setToast({ message: t('node.url_invalid'), type: 'danger' })
+          return
+        }
+
         onSubmit({ alias, url })
       }}
     >
       <div>
-        <label>Alias</label>
-        <input value={alias} onChange={e => setAlias(e.target.value)} />
+        <label htmlFor="alias">{t('node.alias')}</label>
+        <input name="alias" type="text" defaultValue={initialData?.alias} />
       </div>
 
       <div>
-        <label>RPC URL</label>
-        <input value={url} onChange={e => setUrl(e.target.value)} />
+        <label htmlFor="url">{t('node.rpc_url')}</label>
+        <input name="url" type="text" defaultValue={initialData?.url} />
       </div>
 
       {children}
@@ -50,6 +80,7 @@ export const CKBNodeModal = ({ onClose }: { onClose: () => void }) => {
   const { isActivated, setIsActivated, nodeService, nodes, addNode, removeNode, switchNode, editNode } = useCKBNode()
   const [panel, setPanel] = useState<'main' | 'add' | 'edit'>('main')
   const [editIndex, setEditIndex] = useState(0)
+  const setToast = useSetToast()
 
   const NodeEditPanel = () => (
     <div className={styles.contentWrapper}>
@@ -67,7 +98,12 @@ export const CKBNodeModal = ({ onClose }: { onClose: () => void }) => {
       <NodeEditForm
         initialData={nodes[editIndex]}
         onSubmit={data => {
-          editNode(editIndex, data)
+          const success = editNode(editIndex, data)
+          if (!success) {
+            setToast({ message: t('node.node_existed'), type: 'danger' })
+            return
+          }
+
           setPanel('main')
         }}
       >
@@ -102,12 +138,19 @@ export const CKBNodeModal = ({ onClose }: { onClose: () => void }) => {
         {nodes.map((n, index) => (
           <div className={styles.node} key={n.url}>
             <div className={styles.nodeTitle}>
-              <input onClick={() => switchNode(n.url)} type="checkbox" checked={nodeService.nodeEndpoint === n.url} />
+              <input
+                onClick={() =>
+                  switchNode(n.url).catch((err: Error) => setToast({ message: err.message, type: 'danger' }))
+                }
+                type="checkbox"
+                checked={nodeService.nodeEndpoint === n.url}
+              />
               <div className={styles.nodeAlias}>{n.alias}</div>
               {index !== 0 && (
                 <>
-                  {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events */}
-                  <span
+                  <button
+                    className={styles.nodeAction}
+                    type="button"
                     onClick={() => {
                       setPanel('edit')
                       setEditIndex(index)
@@ -115,11 +158,10 @@ export const CKBNodeModal = ({ onClose }: { onClose: () => void }) => {
                     style={{ marginLeft: 'auto' }}
                   >
                     <EditIcon width={16} />
-                  </span>
-                  {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events */}
-                  <span onClick={() => removeNode(n.url)}>
+                  </button>
+                  <button className={styles.nodeAction} type="button" onClick={() => removeNode(n.url)}>
                     <DeleteIcon width={16} />
-                  </span>
+                  </button>
                 </>
               )}
             </div>
@@ -149,7 +191,12 @@ export const CKBNodeModal = ({ onClose }: { onClose: () => void }) => {
 
       <NodeEditForm
         onSubmit={data => {
-          addNode({ alias: data.alias, url: data.url })
+          const success = addNode({ alias: data.alias, url: data.url })
+          if (!success) {
+            setToast({ message: t('node.node_existed'), type: 'danger' })
+            return
+          }
+
           setPanel('main')
         }}
       >
--- src/hooks/useCKBNode.tsx
+++ src/hooks/useCKBNode.tsx
@@ -1,7 +1,10 @@
 import { useContext, createContext, useState, PropsWithChildren } from 'react'
+import { useTranslation } from 'react-i18next'
+import { isMainnet } from '../utils/chain'
 import { NodeService } from '../services/NodeService'
 
 const NODE_CONNECT_MODE_KEY = 'node_connect_mode'
+const NODE_CONNECTED_ENDPOINT = 'node_connected_endpoint'
 const NODE_ENDPOINTS = 'node_endpoints'
 
 const loadEndpoints = (): CKBNode[] => {
@@ -26,10 +29,10 @@ export interface ICKBNodeContext {
   nodes: CKBNode[]
   nodeService: NodeService
   isActivated: boolean
-  addNode: (node: CKBNode) => void
+  addNode: (node: CKBNode) => boolean
   removeNode: (url: string) => void
-  editNode: (index: number, node: CKBNode) => void
-  switchNode: (url: string) => void
+  editNode: (index: number, node: CKBNode) => boolean
+  switchNode: (url: string) => Promise<void>
   setIsActivated: (isActivated: boolean) => void
 }
 
@@ -48,8 +51,10 @@ interface CKBNodeProviderProps {
 }
 
 export const CKBNodeProvider = ({ children, defaultEndpoint }: PropsWithChildren<CKBNodeProviderProps>) => {
-  const [nodeService, setNodeService] = useState(new NodeService(defaultEndpoint))
-  const [nodes, _setNodes] = useState<CKBNode[]>(loadEndpoints())
+  const { t } = useTranslation()
+  const connectedEndpoint = localStorage.getItem(NODE_CONNECTED_ENDPOINT) ?? defaultEndpoint
+  const [nodeService, setNodeService] = useState(new NodeService(connectedEndpoint))
+  const [nodes, setNodes] = useState<CKBNode[]>(loadEndpoints())
   const [isActivated, _setIsActivated] = useState(localStorage.getItem(NODE_CONNECT_MODE_KEY) === 'true')
 
   const setIsActivated = (value: boolean) => {
@@ -57,28 +62,47 @@ export const CKBNodeProvider = ({ children, defaultEndpoint }: PropsWithChildren
     _setIsActivated(value)
   }
 
-  const setNodes = (nodes: CKBNode[]) => {
-    _setNodes(nodes)
+  const saveNodes = (nodes: CKBNode[]) => {
+    setNodes(nodes)
     saveEndpoints(nodes)
   }
 
   const addNode = (node: CKBNode) => {
-    setNodes([...nodes, node])
+    const existed = nodes.some(n => n.url === node.url)
+    if (existed) return false
+
+    saveNodes([...nodes, node])
+    return true
   }
 
   const removeNode = (url: string) => {
-    setNodes(nodes.filter(node => node.url !== url))
+    saveNodes(nodes.filter(node => node.url !== url))
   }
 
   const editNode = (index: number, node: CKBNode) => {
     const newNodes = [...nodes]
-    if (!newNodes[index - 1]) return
+    if (!newNodes[index - 1]) return false
+
+    const existed = newNodes.some((n, i) => n.url === node.url && i !== index - 1)
+    if (existed) return false
+
     newNodes[index - 1] = node
-    setNodes(newNodes)
+    saveNodes(newNodes)
+    return true
   }
 
-  const switchNode = (url: string) => {
-    setNodeService(new NodeService(url))
+  const switchNode = async (url: string) => {
+    const service = new NodeService(url)
+    const res = await service.rpc.getBlockchainInfo().catch(() => {
+      throw new Error(t('node.connect_failed'))
+    })
+
+    if ((res.chain === 'ckb' && !isMainnet()) || (res.chain === 'ckt' && isMainnet())) {
+      throw new Error(t('node.invalid_network'))
+    }
+
+    localStorage.setItem(NODE_CONNECTED_ENDPOINT, url)
+    setNodeService(service)
   }
 
   return (
@@ -86,7 +110,7 @@ export const CKBNodeProvider = ({ children, defaultEndpoint }: PropsWithChildren
       value={{
         nodes: [
           {
-            alias: 'Default Node',
+            alias: t('node.default_node'),
             url: defaultEndpoint,
           },
           ...nodes,
