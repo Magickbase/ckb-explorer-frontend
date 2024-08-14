f9d3aa69ebc8cd79131f195dbd6e010cdc24b6b9
daryl <sai9375@hotmail.com>
2024-08-14 16:20:36 +0800

    fix(rgbdigest): loading

--- src/components/AwesomeLoadings/SmallSpinner/index.tsx
+++ src/components/AwesomeLoadings/SmallSpinner/index.tsx
@@ -2,10 +2,6 @@ import styles from './styles.module.scss'
 import { ReactComponent as SmallSpinner } from '../../../assets/small-spinner.svg'
 
 const Loading = () => {
-  return (
-    <div className={styles.container}>
-      <SmallSpinner />
-    </div>
-  )
+  return <SmallSpinner className={styles.container} />
 }
 export default Loading
