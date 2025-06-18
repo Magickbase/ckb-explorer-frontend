6257c09db4488d4bc16efbb326819dc5b0f16a66
mhcgrq <misaka@magickbase.com>
2025-06-18 10:00:30 +0800

    remove styled-components: wip

--- src/components/HashTag/index.tsx
+++ src/components/HashTag/index.tsx
@@ -1,11 +1,11 @@
 import { useTranslation } from 'react-i18next'
 import { Link } from '../Link'
 import { TYPE_ID_TAG, TYPE_ID_RFC } from '../../utils/typeid'
-import { TagPanel } from './styled'
 import { scripts } from '../../pages/ScriptList'
 import { Script } from '../../models/Script'
 import { ReactComponent as OpenSourceIcon } from '../../assets/open-source.svg'
 import Tooltip from '../Tooltip'
+import styles from './index.module.scss'
 
 export default ({
   content,
@@ -19,16 +19,16 @@ export default ({
   const { t } = useTranslation()
   if (content === TYPE_ID_TAG) {
     return (
-      <TagPanel isLock={category === 'lock'}>
+      <div className={category === 'lock' ? `${styles.tagPanel} ${styles.isLock}` : styles.tagPanel}>
         <Link to={TYPE_ID_RFC} rel="noopener noreferrer" target="_blank" style={{ color: '#000' }}>
           {content}
         </Link>
-      </TagPanel>
+      </div>
     )
   }
   const codeUrl = scripts.get(content)?.code
   return (
-    <TagPanel isLock={category === 'lock'}>
+    <div className={category === 'lock' ? `${styles.tagPanel} ${styles.isLock}` : styles.tagPanel}>
       <Link
         to={`/script/${script.codeHash}/${script.hashType}`}
         rel="noopener noreferrer"
@@ -38,6 +38,6 @@ export default ({
         {content}
         {codeUrl ? <Tooltip trigger={<OpenSourceIcon />}>{t(`scripts.open_source_script`)}</Tooltip> : null}
       </Link>
-    </TagPanel>
+    </div>
   )
 }
